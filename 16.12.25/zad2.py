d4a951d4dfe9ecaea27e6da5f031af7e6ae5dc04
Kir4o-code <kiril.kirilov.eliseev@gmail.com>
2025-12-16 19:08:55 +0200

    12.12.25 hw fix

--- 16.12.25/zad2.py
+++ 16.12.25/zad2.py
@@ -4,8 +4,11 @@ class LibraryAccount:
         self.borrowed_books=[]
     
     def borrow_book(self,book_name : str):
-        self.borrowed_books.append(book_name)
-        print(f"Borrowed book {book_name}.")
+        if len(self.borrowed_books)<3:
+            self.borrowed_books.append(book_name)
+            print(f"Borrowed book {book_name}.")
+        else:
+            print("You cant borrow more than 3 books.")
 
     def return_book(self,book_name: str):
         if book_name in self.borrowed_books:
@@ -23,6 +26,8 @@ class LibraryAccount:
 
 a=LibraryAccount("a")
 a.borrow_book("1984")
+a.borrow_book("48 rules of power")
+a.borrow_book("principia matematica")
 a.list_books()
 a.return_book("1984")
 a.list_books()
